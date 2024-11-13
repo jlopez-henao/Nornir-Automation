afedde32a29d98c26edf04630d49b9313a234a31
Jose Henao <jose.henao@framestore.com>
2024-11-13 11:01:08 +0000

    adding runbook4 getter

---
+++ runbook4_getter.py
@@ -0,0 +1,14 @@
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def pull_interfaces_info(task):
+    interfaces_result = task.run(task=napalm_get, getters= ["get_interfaces"])
+
+results = nr.run(task=pull_interfaces_info)
+
+# import ipdb
+# ipdb.set_trace()
+print_result(results)
